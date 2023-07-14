72eb21a153ed72dd7101e48ed2e4ba5b17cb96ac
Laurence Santos <lsantos@highlysucceed.com>
2023-07-14 21:36:33 +0800

    contact us unfinished

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,14 +1,25 @@
+import './ContactUs.css'
 export default function ContactUs () {
     return(
         <div className="contactus">
+            <div className="contactus-bg">
                 <h1>Hello there...</h1>
-                <label>Name</label>
-                <input type="text"/>
-                <label>Email</label>
-                <input type="text"/>
-                <label>Message</label>
-                <input type="text"/>
-                <button>Submit</button>
+                <div className='message'>
+                    <div className='contactus-container'>
+                        <label>Name: </label>
+                        <input type="text"/>
+                        <label>Email: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className='contactus-container'>
+                        <label>Message: </label>
+                        <textarea/>
+                    </div>
+                    <div className='contactus-container'>
+                        <button>Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
