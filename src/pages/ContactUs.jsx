8a9e10ad16867139571b4f492618db01127a8812
Laurence Santos <lsantos@highlysucceed.com>
2023-07-15 15:26:41 +0800

    bg

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import './ContactUs.css'
 export default function ContactUs () {
     return(
         <div className="contactus">
-            <div className="contactus-bg" style={{backgroundImage: `url('pokemon-bg2.jpg')`}}>
+            <div className="contactus-bg" style={{backgroundImage: `linear-gradient( to right, #19224f, transparent) , url('pokemon-bg2.jpg')`}}>
                 <h1>Hello there...</h1>
                 <div className='message'>
                     <div className='contactus-container'>
