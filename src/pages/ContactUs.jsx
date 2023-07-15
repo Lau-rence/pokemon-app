8f9955b1303de36e7fa004fae61d903e6c9fbbc4
Laurence Santos <lsantos@highlysucceed.com>
2023-07-15 16:11:03 +0800

    added redirect file

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,13 +7,13 @@ export default function ContactUs () {
                 <div className='message'>
                     <div className='contactus-container'>
                         <label>Name: </label>
-                        <input type="text"/>
+                        <input type="text" placeholder='Enter your name here...'/>
                         <label>Email: </label>
-                        <input type="text"/>
+                        <input type="text" placeholder='Enter your email here...'/>
                     </div>
                     <div className='contactus-container'>
                         <label>Message: </label>
-                        <textarea/>
+                        <textarea placeholder='Enter you message here...'/>
                     </div>
                     <div className='contactus-container'>
                         <button>Submit</button>
